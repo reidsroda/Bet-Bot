dba5e11b1351fe69900a42c4666b971845147a0d
Reid Sroda <rgsroda@wisc.edu>
2024-10-07 19:08:45 -0500

    DO NOT RUN THIS CODE
    Will auto place wagers on games that dont even fit the model, needed to stop working to watch my accidental wagers

--- Total_Scraper.py
+++ Total_Scraper.py
@@ -20,6 +20,7 @@ driver.maximize_window()
 username = "ACR75470"
 password = "P9N7"
 wait = WebDriverWait(driver, 30)
+bet_amount = 15.61
 
 #Data Storage
 df = pd.DataFrame(columns = ["Team1", "Team2", "Over_Line", "Over_Odds", "Under_Line", "Under_Odds", "Time"])
@@ -31,7 +32,6 @@ driver.find_element(By.NAME, "Password").send_keys(password)
 time.sleep(2)
 driver.find_element(By.CSS_SELECTOR, '.btn.btn-primary').click()
 time.sleep(5)
-
 
 #Enter Live Betting Interface
 driver.find_element(By.XPATH, '/html/body/div[1]/div/header/div[2]/div[1]/div[2]').click()
@@ -56,19 +56,41 @@ for i in range(10):
                 lines = live_line.find_elements(By.XPATH, ".//*[contains(@class, 'pull')]")
                 if len(lines) == 4:
                     if lines[0].text:
-                        new_row = {"Team1": teams[0].text.split(" ")[-1], "Team2": teams[1].text.split(" ")[-1], "Over_Line": float(lines[0].text[1:]), "Over_Odds": float(lines[1].text), "Under_Line": float(lines[2].text[1:]), "Under_Odds": float(lines[3].text), "Time": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))}
+                        team1 = teams[0].text.split(" ")[-1]
+                        team2 = teams[1].text.split(" ")[-1]
+                        Over_Line = float(lines[0].text[1:])
+                        Over_Odds = float(lines[1].text)
+                        Under_Line = float(lines[2].text[1:])
+                        Under_Odds = float(lines[3].text)
+                        Time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+                        #Prematch_Total = Prematch_Totals.loc[Prematch_Totals["Team1"] == teams[0].text.split(" ")[-1], "Original_Total"][0]
+                        Prematch_Total = 0
+                        #new_row = {"Team1": team1, "Team2": team2, "Over_Line": Over_Line, "Over_Odds": Over_Odds, "Under_Line": Under_Line, "Under_Odds": Under_Odds, "Time": Time, "Prematch_Total": Prematch_Total}
+                        new_row = {"Team1": team1, "Team2": team2, "Over_Line": Over_Line, "Over_Odds": Over_Odds, "Under_Line": Under_Line, "Under_Odds": Under_Odds, "Time": Time}
                         df = df.append(new_row, ignore_index = True)
-                        merged_df = pd.merge(df, Prematch_Totals)
-                        #merged_df = pd.merge(df, Prematch_Totals, on=['Team1', 'Team2'])
+                        if abs(Over_Line - Prematch_Total) > 13:
+                            if Over_Line > Prematch_Total:
+                                #Bet Under
+                                lines[3].click()
+                                time.sleep(5)
+                                bet_slide = driver.find_element(By.CLASS_NAME, "bet-list")
+                                bet_slide.find_element(By.CLASS_NAME, "form-control").send_keys(bet_amount)
+                                time.sleep(2)
+                                driver.find_element(By.XPATH, "//*[@id='bet-slip-process-all']").click()
+                            else:
+                                #Bet Over
+                                lines[1].click()
+                                time.sleep(5)
+
+                        
 
 
 df.to_csv("Live_Lines_Database.csv", index = False)
 
 
-
 #Compare Live Matchup Data with Original Data
 #merged_df = pd.merge(df, Prematch_Totals, on=['Team1', 'Team2'])
-merged_df.to_csv("merged_df.csv", index = False)
+#merged_df.to_csv("merged_df.csv", index = False)
 
 
 """
@@ -82,6 +104,8 @@ for i in range(len(merged_df)):
 
 print(Prematch_Totals)
 """       
+      
+     
       
 
           
